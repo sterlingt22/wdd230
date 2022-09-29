1a001ad2df5a6dd3d83287f25e26e1ace27a7b9a
sterlingt22 <sktucker21@byui.edu>
2022-09-29 14:30:55 -0600

    Final commit for design principles

--- Lesson 2/scripts/date.js
+++ Lesson 2/scripts/date.js
@@ -1,77 +1,36 @@
+let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
 
-let today1 = new Date()
-let year = today1.getFullYear();
-let fulldate = `${year}`;
-document.querySelector("#year").textContent = fulldate;
+let months = [
+    "January",
+    "Februray",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
 
-let lastmod = new Date(document.lastModified)
-document.querySelector("#updated").textContent = lastmod;
+let d = new Date();
 
+let dayName = days[d.getDay()];
+let monthName = months[d.getMonth()];
+let year = d.getFullYear();
 
-let todaystr = "";
-switch (today1.getDay()) {
-    case 0:
-        todaystr = "Sunday";
-        break;
-    case 1:
-        todaystr = "Monday";
-        break;
-    case 2:
-        todaystr = "Tuesday";
-        break;
-    case 3:
-        todaystr = "Wednesday";
-        break;
-    case 4:
-        todaystr = "Thursday";
-        break;
-    case 5:
-        todaystr = "Friday";
-        break;
-    case 6:
-        todaystr = "Saturday";
-        break;
-}
-let month = "";
+let fullDate = dayName + ", " + monthName + " " + d.getDate() + ", " + year;
 
-switch (today1.getMonth()) {
-    case 0:
-        month = "January";
-        break;
-    case 1:
-        month = "February";
-        break;
-    case 2:
-        month = "March";
-        break;
-    case 3:
-        month = "April";
-        break;
-    case 4:
-        month = "May";
-        break;
-    case 5:
-        month = "June";
-        break;
-    case 6:
-        month = "July";
-        break;
-    case 7:
-        month = "August";
-        break;
-    case 8:
-        month = "September";
-        break;
-    case 9:
-        month = "October";
-        break;
-    case 10:
-        month = "November";
-        break;
-    case 11:
-        month = "December";
-        break;
-}
+document.querySelector('#year').innerHTML = year;
 
-let todayconcat = todaystr + ", " + today1.getDate() + " " + month + " " + year;
-document.querySelector("#today").textContent = todayconcat;
+document.querySelector('#lastMod').innerHTML = fullDate;
