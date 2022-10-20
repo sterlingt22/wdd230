1722cfce6114669daaef9c59c7a402889308ed27
sterlingt22 <sktucker21@byui.edu>
2022-10-20 15:42:18 -0600

    WK5 Chamber home page continued

--- chamber/scripts/menu_open.js
+++ chamber/scripts/menu_open.js
@@ -1,13 +1,11 @@
-// first we create a function with the name toggleMenu
 function toggleMenu() {
-    // console.log("It worked!")
-    // we locate an element with a ID name of primaryNav (ul), and toggle the classList
+    
     document.getElementById("ulList").classList.toggle("open");
-    // // To add the 'X' inplace of the hamburger button.
+    
     document.getElementById("button").classList.toggle("open");
   }
   
-  // call the function toogleMenu
   const x = document.getElementById("button");
   x.onclick = toggleMenu;
-  
+
+  const banner = document.querySelector(".banner");
