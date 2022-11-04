2ebec58bcfdcaa7ad356b67fcda0c788463566a5
sterlingt22 <sktucker21@byui.edu>
2022-11-04 10:29:09 -0600

    table build update

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -4,7 +4,7 @@ function chillF(temperature, windSpeed) {
   const windS = document.getElementById("windspeed").textContent;
   const wind = parseInt(windS);
   if (temp <= 50 && wind > 3) {
-    const chilly = (35.74 + (0.6215 * temp) - (35.75 * wind**0.16) + (0.4275 * temp * wind ** 0.16)).toFixed(1);
+    const chilly = (35.74 + (0.6215 * temp) - (35.75 * wind**0.16) + (0.4275 * temp * wind ** 0.16)).toFixed(0);
     return chilly + `°F`;
   } else{
     return "N/A"
@@ -13,18 +13,4 @@ function chillF(temperature, windSpeed) {
 
 document.getElementById("windchill").innerHTML = chillF(temperatureF, windspeed);
 
-
-
-// function doInputOutput() {
-  
-//   let tempF = parseFloat(document.getElementById("temperatureF").value);
-//   let speed = parseFloat(document.getElementById("windSpeedInput").value);
-//   finalTemp = windChill(tempF, speed);
-//   document.getElementById("outputDiv").innerHTML = finalTemp;
-// }
-
-//   function windchill(tempF, speed) {
-//     let windChill = 35.74 + (0.6215 * tempF) - (35.75 * speed**0.16) + (0.4275 * tempF * speed ** 0.16);
-//     return windChill;
-//   }
 
